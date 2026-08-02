ce847a75245f147e882515d422fc259499f1f8d1
focusforgehq4 <focusforgehq4@gmail.com>
2026-08-02 17:26:26 +0100

    Update led_engine.h

--- docs/firmware/led_engine.h
+++ docs/firmware/led_engine.h
@@ -1,1 +1,20 @@
+#ifndef LED_ENGINE_H
+#define LED_ENGINE_H
 
+void ledSetup();
+
+void ledIdle();
+
+void ledStartup();
+
+void ledFocus();
+
+void ledWarning();
+
+void ledBreak();
+
+void ledFinished();
+
+void ledOff();
+
+#endif
